3641e5bf1633fbbfb921d8e335451f5118ad5039
supritkumar shah <suprit7@gmail.com>
2016-07-19 17:51:36 -0400

    Directory is up and server is running

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict'
+// const env         = process.env.NODE_ENV || 'development';
+// const DEV         = env==='development';
+// const dotenv      = (DEV) ? require('dotenv').config() : undefined;
+
+
+
+const express     = require('express')
+const path        = require('path')
+const logger      = require('morgan')
+const bodyParser  = require('body-parser')
+const PORT        = process.env.PORT ||3000
+const app         = express()
+
+
+
+
+
+app.use(logger('dev'));
+// app.use(logger(DEV ? 'dev' : 'common'))
+app.use(express.static(path.join(__dirname,'dist')))
+app.use(bodyParser.json())
+
+
+
+app.get('/', (req, res)=>{
+  res.send('Hello Home')
+})
+
+
+// app.use('/movie', movieRoutes);
+
+
+app.listen(PORT, ()=>{
+  console.log('Server is listening on port', PORT);
+})
